d3409331da4f09da9c97ad420ae0f0a42460f0d8
Alexander Ignácz <ignaczalexander@gmail.com>
2019-11-11 11:10:47 +0100

    Add publicPath to config

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,6 @@ module.exports = {
         prependData: `@import "@/assets/styles/_variables.scss";`
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/patiro_task/' : '/'
 };
